0775da22a441c023e145f58f95d70843ca1216ee
Sogun <sogun.kwn@gmail.com>
2019-04-03 22:12:48 +0800

    Update NewUserSeeder.php

--- database/seeds/NewUserSeeder.php
+++ database/seeds/NewUserSeeder.php
@@ -8,17 +8,12 @@ class NewUserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'sogun',
+            'name' => 'admin',
             'email' => 'admin@gmail.com',
             'type' =>'admin',
             'password' => bcrypt('secret'),
         ]);
 
-        DB::table('users')->insert([
-            'name' => 'dedy',
-            'email' => 'peminjam@gmail.com',
-            'type' =>'peminjam',
-            'password' => bcrypt('secret'),
-        ]);
+      
     }
 }
